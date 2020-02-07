c0e9ddfce32846d29af876d8a20ff6b990d52424
jeremysmorris <jeremysmorris@gmail.com>
2020-02-07 12:17:58 -0500

    Added code for Coursera JHU R Progamming Course, Programming Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions calculate the inverse of matrix x and cache it in the global
+## environment.
 
-## Write a short comment describing this function
+## This function stores a matrix x in the cache, clears any value of s that had
+## been cached by a prior execution of cacheSolve(), and defines and names
+## getting and setting functions to be used by cacheSolve.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        s <- NULL
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) s <<- solve
+        getsolve <- function() s
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+## This function calculates s (the inverse of matrix x) and caches it in the 
+## global environent. It also first checks to see if a cached value of s is
+## already present in the global environment; if there is, the function will 
+## return the message "getting cached data", meaning that no new matrix x has
+## been passed through makeCacheMatrix().
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        s <- x$getsolve()
+        if(!is.null(s)) {
+                message("getting cached data")
+                return(s)
+        }
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setsolve(s)
+        s
 }
